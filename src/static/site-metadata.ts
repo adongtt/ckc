52ca2ddfddbcd007922b498decc2438ca715867a
adongtt <240732222@qq.com>
2024-01-10 16:27:14 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: "强锅 的 Running Page",
+  siteUrl: 'https://adongtt.github.io/ckc/',
+  logo: 'https://www.custombaseballgloves.com/static/images/ckc.jpg',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://adongtt.github.io/ckc/',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://adongtt.github.io/ckc/',
     },
   ],
 };
